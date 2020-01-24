45c47c1e6fa745eae66f3e5595494af1027321da
77artur77 <arturhunia@gmail.com>
2020-01-24 20:52:09 +0100

    i added some funcions

--- app/src/main/java/com/example/myapplication/Controller/FinishActivity.kt
+++ app/src/main/java/com/example/myapplication/Controller/FinishActivity.kt
@@ -3,11 +3,19 @@ package com.example.myapplication.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.Utilities.EXTRA_LEAGUE
+import com.example.myapplication.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_finish.*
 
-class FinishActivity : AppCompatActivity() {
+class FinishActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        val skill = intent.getStringExtra(EXTRA_SKILL)
+
+        searchLeaguesText.text = "Looking for $league $skill league near  you..."
     }
 }
--- app/src/main/java/com/example/myapplication/Controller/SkillsActivity.kt
+++ app/src/main/java/com/example/myapplication/Controller/SkillsActivity.kt
@@ -3,12 +3,15 @@ package com.example.myapplication.Controller
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.myapplication.Utilities.EXTRA_LEAGUE
 import com.example.myapplication.R
+import com.example.myapplication.Utilities.EXTRA_SKILL
 import kotlinx.android.synthetic.main.activity_skills.*
 
 class SkillsActivity : BaseActivity() {
     var league = ""
+    var skill = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skills)
@@ -16,15 +19,23 @@ class SkillsActivity : BaseActivity() {
         println(league)
     }
     fun onSkillFinishClicked(view: View){
-        val finishActivity = Intent(this, FinishActivity::class.java)
-        startActivity(finishActivity)
+        if (skill != "") {
+            val finishActivity = Intent(this, FinishActivity::class.java)
+            finishActivity.putExtra(EXTRA_LEAGUE, league)
+            finishActivity.putExtra(EXTRA_SKILL, skill)
+            startActivity(finishActivity)
+        } else {
+            Toast.makeText(this, "Please choose a skill", Toast.LENGTH_SHORT).show()
+        }
+
     }
     fun onBeginnerClicked(view: View){
-
+        ballerSkillsBtn.isChecked = false
+        skill = "beginner"
     }
     fun onBallerClicked(view: View){
         beginnerSkillsBtn.isChecked = false
+        skill = "baller"
     }
-
 
 }
