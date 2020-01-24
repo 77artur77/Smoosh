b282a7977bbb08fb1097c9444364efe3d590cd43
77artur77 <arturhunia@gmail.com>
2020-01-24 17:28:45 +0100

    i added now kotlin avtivity

---
+++ app/src/main/java/com/example/myapplication/Controller/ss.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ss : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ss)
+    }
+}
