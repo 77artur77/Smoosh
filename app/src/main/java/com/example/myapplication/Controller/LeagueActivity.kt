52b3cd8e480d065c24cbd1089da9644fe40716c7
77artur77 <arturhunia@gmail.com>
2020-01-24 23:53:57 +0100

    i refactored (add Model class player)

--- app/src/main/java/com/example/myapplication/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/myapplication/Controller/LeagueActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.myapplication.Utilities.EXTRA_LEAGUE
+import com.example.myapplication.Model.Player
 import com.example.myapplication.R
+import com.example.myapplication.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
     //var changeable variable//
-    var selectedLeague = ""
+    var player = Player("","")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -18,24 +20,24 @@ class LeagueActivity : BaseActivity() {
     fun onMensClicked(view: View) {
         womensLeagueBtn.isChecked = false
         coedLeaugeBtn.isChecked = false
-        selectedLeague = "mens"
+        player.league = "mens"
     }
     fun onWomensClicked(view: View) {
         mensLeagueBtn.isChecked = false
         coedLeaugeBtn.isChecked = false
-        selectedLeague = "womens"
+        player.league = "womens"
     }
     fun onCoedClicked(view: View) {
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
-        selectedLeague = "Co - Ed"
+        player.league = "Co - Ed"
     }
 
 
     fun leagueNextClicked(view: View){
-        if (selectedLeague != "") {
+        if (player.league != "") {
             val skillsActivity = Intent(this, SkillsActivity::class.java)
-            skillsActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillsActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillsActivity)
         } else {
             Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/myapplication/Controller/SkillsActivity.kt
+++ app/src/main/java/com/example/myapplication/Controller/SkillsActivity.kt
@@ -4,25 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.myapplication.Utilities.EXTRA_LEAGUE
+import com.example.myapplication.Model.Player
 import com.example.myapplication.R
-import com.example.myapplication.Utilities.EXTRA_SKILL
+import com.example.myapplication.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_skills.*
 
 class SkillsActivity : BaseActivity() {
-    var league = ""
-    var skill = ""
+    lateinit var player : Player
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skills)
-        league = intent.getStringExtra(EXTRA_LEAGUE)
-        println(league)
+        player = intent.getParcelableExtra(EXTRA_PLAYER)
+        println(player)
     }
     fun onSkillFinishClicked(view: View){
-        if (skill != "") {
+        if (player.skill != "") {
             val finishActivity = Intent(this, FinishActivity::class.java)
-            finishActivity.putExtra(EXTRA_LEAGUE, league)
-            finishActivity.putExtra(EXTRA_SKILL, skill)
+            finishActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(finishActivity)
         } else {
             Toast.makeText(this, "Please choose a skill", Toast.LENGTH_SHORT).show()
@@ -31,11 +30,11 @@ class SkillsActivity : BaseActivity() {
     }
     fun onBeginnerClicked(view: View){
         ballerSkillsBtn.isChecked = false
-        skill = "beginner"
+        player.skill = "beginner"
     }
     fun onBallerClicked(view: View){
         beginnerSkillsBtn.isChecked = false
-        skill = "baller"
+        player.skill = "baller"
     }
 
 }
