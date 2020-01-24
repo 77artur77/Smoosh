2b5ea429bd1604cf7adeb8e191756adbd7be188a
77artur77 <arturhunia@gmail.com>
2020-01-24 15:51:10 +0100

    change mens to Mens in League Activiity

--- app/src/main/java/com/example/myapplication/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/myapplication/Controller/LeagueActivity.kt
@@ -18,7 +18,7 @@ class LeagueActivity : BaseActivity() {
     fun onMensClicked(view: View) {
         womensLeagueBtn.isChecked = false
         coedLeaugeBtn.isChecked = false
-        selectedLeague = "mens"
+        selectedLeague = "Mens"
     }
     fun onWomensClicked(view: View) {
         mensLeagueBtn.isChecked = false
